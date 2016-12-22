9bc778c5d2e784f1fb187b8ca6e2712f705e5316
Venus Garg <garg.venus39@gmail.com>
2016-12-22 08:09:16 +0000

    Added components for login

--- client/src/js/main.jsx
+++ client/src/js/main.jsx
@@ -1,1 +1,31 @@
 import 'file?name=[name].[ext]!../index.html';
+import React from 'react';
+import ReactDOM from 'react-dom';
+var {browserHistory,hashHistory, Route, Router, IndexRoute} = require('react-router');
+
+import About from './components/About.jsx';
+import Contact from './components/Contact.jsx';
+import NavBar from './components/NavBar.jsx';
+import registrationAndLogin from './components/registrationAndLogin.jsx';
+
+class MainComponent extends React.Component {
+
+	render() {
+		return (
+		<div>
+		<NavBar/>
+		<br/><br/><br/><br/>
+		{this.props.children}
+		</div>
+		)
+	}
+}
+ReactDOM.render(
+	<Router history={browserHistory}>
+	<Route path="/" component={MainComponent} >
+		<IndexRoute component = {registrationAndLogin} />
+		<Route path="/about" component={About}/>
+		<Route path="/contact" component={Contact}/>
+	</Route>
+
+</Router>,document.getElementById('content'));
